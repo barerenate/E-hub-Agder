d568bfacdf4312896e0c97eedcaad6027ba08bef
tessybonsnes <tessy.caroline.bonsnaes@kristiansand.kommune.no>
2024-05-28 11:16:14 +0200

    Update Apply.tsx

--- src/pages/Apply.tsx
+++ src/pages/Apply.tsx
@@ -6,7 +6,7 @@ function Apply() {
       <div className="apply-container">
         <div className="microsoft-form-container">
           <iframe
-          src="https://forms.office.com/Pages/ResponsePage.aspx?id=HoiChJk2P0uxNc9IALwe-7I9EHAzPl5Lgc3v-z0l089UOERFSDZQWVlUQ1I0T1RFMzhGNVJETlg1WS4u&embed=true"
+          src="https://forms.office.com/Pages/ResponsePage.aspx?id=zhhMsEn-g0OCNUfzlfB0dLToqTi8ui1IgQZp9eKJhpNUNk9RRVE5Wk1DNUQzSTdENTlDOEZENlEzRC4u&embed=true"
           allowFullScreen
           />
       </div>
